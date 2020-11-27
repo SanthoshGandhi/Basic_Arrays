eef9c011616ed0f1e2a004c467c4de3dd0046e51
SanthoshGandhi <74761021+SanthoshGandhi@users.noreply.github.com>
2020-11-27 19:51:58 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,31 +10,31 @@ namespace Array_demo
     {
         static void Main(string[] args)
         {
-            //int[] sample = new int[5] { 1, 2, 3, 4, 5 };
-            //int sum = 0;
-            //string x = "";
-            //for (int i = 0; i < sample.Length; i++)
-            //{
-            //    sum += sample[i];
-            //    x += sample[i] + "\t";
-            //}
-            //Console.WriteLine("array elements : "+x);
-            //Console.WriteLine(string.Format("the sum of the arrays are:{0}",sum));
+            int[] sample = new int[5] { 1, 2, 3, 4, 5 };
+            int sum = 0;
+            string x = "";
+            for (int i = 0; i < sample.Length; i++)
+            {
+                sum += sample[i];
+                x += sample[i] + "\t";
+            }
+            Console.WriteLine("array elements : "+x);
+            Console.WriteLine(string.Format("the sum of the arrays are:{0}",sum));
 
 
-            //int[,] sample1 = new int[2, 5] { { 1, 1, 1, 1, 1 }, {1, 1, 1, 1, 1} };
-            //int sum1 = 0;
-            //string x1 = "";
-            //for (int i = 0; i < sample1.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < sample1.GetLength(1); j++)
-            //    {
-            //        sum1 += sample1[i, j];
-            //        x1 += sample1[i, j] + "\t";
-            //    }    
-            //}
-            //Console.WriteLine("array elements :" + x1);
-            //Console.WriteLine(string.Format("the sumz of the array are:{0}", sum1));
+            int[,] sample1 = new int[2, 5] { { 1, 1, 1, 1, 1 }, {1, 1, 1, 1, 1} };
+            int sum1 = 0;
+            string x1 = "";
+            for (int i = 0; i < sample1.GetLength(0); i++)
+            {
+                for (int j = 0; j < sample1.GetLength(1); j++)
+                {
+                    sum1 += sample1[i, j];
+                    x1 += sample1[i, j] + "\t";
+                }    
+            }
+            Console.WriteLine("array elements :" + x1);
+            Console.WriteLine(string.Format("the sumz of the array are:{0}", sum1));
 
             int[][] sample2 = new int[3][];
             sample2[0] = new int[4] { 1, 2, 3, 4 };
